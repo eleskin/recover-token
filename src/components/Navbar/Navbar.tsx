87012ce96e696d3f0871d8e303f180ab0c760223
aleksandreleskin <eleskin1201@gmail.com>
2021-05-12 21:18:26 +0300

    success ui

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,24 +14,28 @@ const Navbar = () => {
       <Link
         to="/"
         className={location.pathname === '/' ? styles.Navbar__link_active : styles.Navbar__link}
+        data-text="Stacking LP"
       >
         <FontAwesomeIcon icon={faLayerGroup}/>
       </Link>
       <Link
         to="/migration"
         className={location.pathname === '/migration' ? styles.Navbar__link_active : styles.Navbar__link}
+        data-text="Migration"
       >
         <FontAwesomeIcon icon={faExchangeAlt}/>
       </Link>
       <Link
         to="/white-paper"
         className={location.pathname === '/white-paper' ? styles.Navbar__link_active : styles.Navbar__link}
+        data-text="White paper"
       >
         <FontAwesomeIcon icon={faStickyNote}/>
       </Link>
       <Link
         to="/about"
         className={location.pathname === '/about' ? styles.Navbar__link_active : styles.Navbar__link}
+        data-text="About stats"
       >
         <FontAwesomeIcon icon={faInfo}/>
       </Link>
--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -1,9 +1,42 @@
 import styles from './Social.module.css';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter, faTelegramPlane, faMediumM, faEthereum} from '@fortawesome/free-brands-svg-icons';
 
 const Social = () => {
   return (
     <div className={styles.Social}>
+      <a
+        href="https://twitter.com"
+        className={styles.Social__link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitter}/>
+      </a>
+      <a
+        href="https://web.telegram.org/"
+        className={styles.Social__link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faTelegramPlane}/>
+      </a>
+      <a
+        href="https://medium.com/"
+        className={styles.Social__link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faMediumM}/>
+      </a>
+      <a
+        href="https://wikipedia.org/wiki/Ethereum"
+        className={styles.Social__link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faEthereum}/>
+      </a>
     </div>
   );
 };
